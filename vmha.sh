be2c912ed6e4c34c7dfe6ef9c3068f8d3f14e7a3
jiangcuo <49061187+jiangcuo@users.noreply.github.com>
2023-01-18 14:19:49 +0800

    Create vmha.sh

---
+++ vmha.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+host="10.13.14.254"
+vmid="122"
+logfile="/var/log/vmha-$vmid.log"
+vmstatus=`qm status $vmid|awk '{print $2}'`
+echo "##############started####################" >> $logfile
+#use ping to check
+ping -c2 -i0.3 -W1 $host &>/dev/null
+if [ $? == 0 ];then
+    echo "`date` $host is on" >> $logfile
+    # check backup vm status
+    if [ $vmstatus == "stopped" ];then
+        echo "`date` backupvm $vmid is off , do nothing " >> $logfile
+    else 
+        echo "`date` backupvm is on, do nothing" >> $logfile
+        qm stop $vmid
+    fi  
+else
+    #if 
+    echo "`date` $host is off" >> $logfile
+    if [ $vmstatus == "stopped" ];then
+        echo "`date` backupvm $vmid is off , turn on" >> $logfile
+        qm start $vmid
+    else 
+        echo "`date` backupvm is on, do nothing" >> $logfile
+    fi  
+fi
+echo "##############End###################" >> $logfile
